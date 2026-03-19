2d8c6f4477b97f18e2641795a10b55611d63406a
Artem K. <artem.kabatov22@gmail.com>
2026-03-19 17:27:21 +0300

    feat(game): add Game class

---
+++ src/model/game/Game.java
@@ -0,0 +1,14 @@
+package model.game;
+
+public class Game {
+    private int moveCount;
+    private Field _field;
+
+    public void start() {
+
+    }
+
+    public boolean isOver() {
+        return false;
+    }
+}
