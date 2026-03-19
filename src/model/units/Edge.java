9f7a7991fc56cc4e613d9e2b69dd4c606163b54a
Artem K. <artem.kabatov22@gmail.com>
2026-03-19 17:27:31 +0300

    feat(units): add Edge class

---
+++ src/model/units/Edge.java
@@ -0,0 +1,11 @@
+package model.units;
+
+public class Edge {
+    private Node _nodeA;
+    private Node _nodeB;
+
+    Edge(Node nodeA, Node nodeB) {
+        _nodeA = nodeA;
+        _nodeB = nodeB;
+    }
+}
