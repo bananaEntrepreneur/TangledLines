8354c7af221061cad0bd039e18ff7e865f1bd64a
Artem K. <artem.kabatov22@gmail.com>
2026-03-23 16:39:16 +0300

    feat(units): add to line method

--- src/model/units/Edge.java
+++ src/model/units/Edge.java
@@ -1,32 +1,28 @@
 package model.units;
 
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 public class Edge {
-    private Node _nodeA;
-    private Node _nodeB;
+    private final Node _nodeA;
+    private final Node _nodeB;
 
     public Edge(Node nodeA, Node nodeB) {
         _nodeA = nodeA;
         _nodeB = nodeB;
     }
 
+    public Line2D toLine() {
+        Point2D a = _nodeA.getPosition();
+        Point2D b = _nodeB.getPosition();
+        return new Line2D.Double(a.getX(), a.getY(), b.getX(), b.getY());
+    }
+
     public boolean hasIntersection(Edge other) {
         if (sharesNodeWith(other)) {
             return false;
         }
-
-        Line2D thisLine = new Line2D.Double(
-            _nodeA.getPosition().getX(), _nodeA.getPosition().getY(),
-            _nodeB.getPosition().getX(), _nodeB.getPosition().getY()
-        );
-
-        Line2D otherLine = new Line2D.Double(
-            other._nodeA.getPosition().getX(), other._nodeA.getPosition().getY(),
-            other._nodeB.getPosition().getX(), other._nodeB.getPosition().getY()
-        );
-
-        return thisLine.intersectsLine(otherLine);
+        return this.toLine().intersectsLine(other.toLine());
     }
 
     private boolean sharesNodeWith(Edge other) {
