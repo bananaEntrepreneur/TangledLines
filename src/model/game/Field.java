1ac7756fbb62554eed5a4147ee58006bbde3d768
Artem K. <artem.kabatov22@gmail.com>
2026-03-23 15:50:27 +0300

    feat(game): add basic implementation

--- src/model/game/Field.java
+++ src/model/game/Field.java
@@ -1,16 +1,56 @@
 package model.game;
 
+import model.listeners.NodeChangeListener;
 import model.units.Edge;
 import model.units.Node;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Field {
     private List<Node> _nodes;
     private List<Edge> _edges;
 
+    public Field() {
+        _nodes = new ArrayList<>();
+        _edges = new ArrayList<>();
+    }
+
+    public void addNode(Node node) {
+        _nodes.add(node);
+        node.addListener((NodeChangeListener) this);
+    }
+
+    public void addEdge(Edge edge) {
+        _edges.add(edge);
+    }
+
+    public List<Node> getNodes() {
+        return new ArrayList<>(_nodes);
+    }
+
+    public List<Edge> getEdges() {
+        return new ArrayList<>(_edges);
+    }
+
     public boolean moveNodeTo(Node node, Point2D newPosition) {
+        if (node == null || newPosition == null || !_nodes.contains(node)) {
+            return false;
+        }
+
+        node.setPosition(newPosition);
         return true;
+    }
+
+    public boolean hasIntersections() {
+        for (int i = 0; i < _edges.size(); i++) {
+            for (int j = i + 1; j < _edges.size(); j++) {
+                if (_edges.get(i).hasIntersection(_edges.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -1,14 +1,37 @@
 package model.game;
 
+import model.units.Node;
+
+import java.awt.geom.Point2D;
+
 public class Game {
     private int moveCount;
-    private Field _field;
+    private final Field _field;
 
-    public void start() {
-
+    public Game(Field field) {
+        _field = field;
+        moveCount = 0;
     }
 
-    public boolean isOver() {
-        return false;
+    public Field getField() {
+        return _field;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void start() { }
+
+    public boolean moveNode(Node node, Point2D newPosition) {
+        boolean moved = _field.moveNodeTo(node, newPosition);
+        if (moved) {
+            moveCount++;
+        }
+        return moved;
+    }
+
+    public boolean hasWon() {
+        return !_field.hasIntersections();
     }
 }
