aee3b37188647c0095a91359342e123f757e378a
Artem K. <artem.kabatov22@gmail.com>
2026-03-23 15:50:41 +0300

    feat(units): add basic implementation

--- src/model/units/Edge.java
+++ src/model/units/Edge.java
@@ -1,11 +1,36 @@
 package model.units;
 
+import java.awt.geom.Line2D;
+
 public class Edge {
     private Node _nodeA;
     private Node _nodeB;
 
-    Edge(Node nodeA, Node nodeB) {
+    public Edge(Node nodeA, Node nodeB) {
         _nodeA = nodeA;
         _nodeB = nodeB;
+    }
+
+    public boolean hasIntersection(Edge other) {
+        if (sharesNodeWith(other)) {
+            return false;
+        }
+
+        Line2D thisLine = new Line2D.Double(
+            _nodeA.getPosition().getX(), _nodeA.getPosition().getY(),
+            _nodeB.getPosition().getX(), _nodeB.getPosition().getY()
+        );
+
+        Line2D otherLine = new Line2D.Double(
+            other._nodeA.getPosition().getX(), other._nodeA.getPosition().getY(),
+            other._nodeB.getPosition().getX(), other._nodeB.getPosition().getY()
+        );
+
+        return thisLine.intersectsLine(otherLine);
+    }
+
+    private boolean sharesNodeWith(Edge other) {
+        return this._nodeA == other._nodeA || this._nodeA == other._nodeB ||
+               this._nodeB == other._nodeA || this._nodeB == other._nodeB;
     }
 }
--- src/model/units/Node.java
+++ src/model/units/Node.java
@@ -1,14 +1,20 @@
 package model.units;
 
+import model.listeners.NodeChangeListener;
+
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node {
     private Point2D _position;
     private final boolean _movable;
+    private final List<NodeChangeListener> _listeners;
 
-    Node(Point2D position, boolean movable) {
+    public Node(Point2D position, boolean movable) {
         _position = position;
         _movable = movable;
+        _listeners = new ArrayList<>();
     }
 
     public Point2D getPosition() {
@@ -19,7 +25,22 @@ public class Node {
         return _movable;
     }
 
-    private void setPosition(Point2D newPosition) {
+    public void addListener(NodeChangeListener listener) {
+        _listeners.add(listener);
+    }
+
+    public void setPosition(Point2D newPosition) {
+        if (!_movable) {
+            return;
+        }
+        Point2D oldPosition = _position;
         _position = newPosition;
+        notifyListeners(oldPosition, newPosition);
+    }
+
+    private void notifyListeners(Point2D oldPosition, Point2D newPosition) {
+        for (NodeChangeListener listener : _listeners) {
+            listener.onNodeMoved(this, oldPosition, newPosition);
+        }
     }
 }
