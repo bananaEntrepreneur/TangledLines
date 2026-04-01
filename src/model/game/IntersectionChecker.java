ec3b48a76760f8d70fe5c6098e8b950d91fbace0
Artem K. <artem.kabatov22@gmail.com>
2026-04-01 21:59:14 +0300

    chore

--- src/model/game/IntersectionChecker.java
+++
@@ -1,9 +0,0 @@
-package model.game;
-
-import model.units.Edge;
-
-import java.util.List;
-
-public interface IntersectionChecker {
-    boolean hasIntersections(List<Edge> edges);
-}
--- src/model/level/LevelLoader.java
+++
@@ -1,7 +0,0 @@
-package model.level;
-
-import java.io.IOException;
-
-public interface LevelLoader {
-    Level load(String source) throws IOException, LevelLoadException;
-}
