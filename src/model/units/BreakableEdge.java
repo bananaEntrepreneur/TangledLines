552f5a55e0d7816efeed37815442152c329c70dd
Artem K. <artem.kabatov22@gmail.com>
2026-05-11 20:47:06 +0300

    feat: add breakable edge

---
+++ src/model/units/BreakableEdge.java
@@ -0,0 +1,55 @@
+package model.units;
+
+import model.listeners.NodeListener;
+
+public class BreakableEdge extends TensionedEdge implements NodeListener {
+    private final double _breakStretchPercent;
+    private boolean _broken = false;
+
+    public BreakableEdge(Node nodeA, Node nodeB, double breakStretchPercent) {
+        super(nodeA, nodeB);
+        if (breakStretchPercent < 0) {
+            throw new IllegalArgumentException("Break percent cannot be negative");
+        }
+        _breakStretchPercent = breakStretchPercent;
+
+        nodeA.addListener(this);
+        nodeB.addListener(this);
+    }
+
+    public double getBreakLength() {
+        return getOriginalLength() * (1.0 + _breakStretchPercent / 100.0);
+    }
+
+    public double getOriginalLength() {
+        return super.getOriginalLength();
+    }
+
+    public double getCurrentLength() {
+        return super.getCurrentLength();
+    }
+
+    public boolean isBroken() {
+        return _broken;
+    }
+
+    public boolean isReadyToBreak() {
+        if (_broken) {
+            return false;
+        }
+
+        double breakLength = getBreakLength();
+        if (breakLength <= 0) {
+            return getCurrentLength() > 0;
+        }
+
+        return getCurrentLength() >= breakLength * 0.9;
+    }
+
+    @Override
+    public void onMoved(Node node) {
+        if (!_broken && getCurrentStretchFactor() >= 1.0 + _breakStretchPercent / 100.0) {
+            _broken = true;
+        }
+    }
+}
